59c2c57e25c462f6b984047b2c7dd69b05b5ec85
Shihemi100 <carren.shihemi@student.moringa.com>
2025-05-26 03:47:01 +0300

    p3-oo-one-to-many-lab

---
+++ lib/owner.py
@@ -0,0 +1,16 @@
+from lib.pet import Pet
+
+class Owner:
+    def __init__(self, name):
+        self.name = name
+
+    def pets(self):
+        return [pet for pet in Pet.all if pet.owner == self]
+
+    def add_pet(self, pet):
+        if not isinstance(pet, Pet):
+            raise Exception("Must add a Pet instance.")
+        pet.owner = self
+
+    def get_sorted_pets(self):
+        return sorted(self.pets(), key=lambda pet: pet.name)
---
+++ lib/pet.py
@@ -0,0 +1,16 @@
+class Pet:
+    PET_TYPES = ["dog", "cat", "rodent", "bird", "reptile", "exotic"]
+    all = []
+
+    def __init__(self, name, pet_type, owner=None):
+        if pet_type not in Pet.PET_TYPES:
+            raise Exception("Invalid pet type.")
+        
+        self.name = name
+        self.pet_type = pet_type
+
+        if owner is not None and not isinstance(owner, Owner):
+            raise Exception("Owner must be an instance of Owner class.")
+        
+        self.owner = owner
+        Pet.all.append(self)
